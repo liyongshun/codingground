0322a5f79a5ff720cbb8a86ff57fd3f0dcbb0af7
liyongshun <yongshunli@163.com>
2016-06-13 20:22:16 +0800

    Update main.cpp
    add print

--- New Project-20160613/main.cpp
+++ New Project-20160613/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-   cout << "Hello World l'm liyongshun" << endl; 
+   cout << "Hello World l'm liyongshun and what's your name?" << endl; 
    
    return 0;
 }
